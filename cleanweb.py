f2eafa22fae17e6282f811947a1270b6950b4add
kazmiruk <kazmiruk@live.ru>
2014-08-01 19:02:52 +0800

    dirty hack for CleanWeb api errors. If it can't response with correct data lib builds fake response

--- cleanweb.py
+++ cleanweb.py
@@ -13,11 +13,12 @@ class Cleanweb(object):
         http://api.yandex.ru/cleanweb
     """
 
-    def __init__(self, key=None):
+    def __init__(self, key=None, fake_captcha=None):
         if not key:
             raise CleanwebError('Cleanweb needs API key to operate. Get it here: http://api.yandex.ru/cleanweb/form.xml')
         self.session = requests.Session()
         self.session.params['key'] = key
+        self.fake_captcha = fake_captcha
 
     def request(self, *args, **kwargs):
         """ Error handling in requests
@@ -25,9 +26,14 @@ class Cleanweb(object):
         """
         r = self.session.request(*args, **kwargs)
         if r.status_code != requests.codes.ok:
-            error = ET.fromstring(r.content)
-            message = error.findtext('message')
-            code = error.attrib['key']
+            try:
+                error = ET.fromstring(r.content)
+                message = error.findtext('message')
+                code = error.attrib['key']
+            except ET.ParseError:
+                message = "Invalid response from server"
+                code = r.status_code
+
             raise CleanwebError('%s (%s)' % (message, code))
         return r
 
@@ -39,18 +45,42 @@ class Cleanweb(object):
         """
         data = {'ip': ip, 'email': email, 'name': name, 'login': login, 'realname': realname,
                 'body-%s' % body_type: body, 'subject-%s' % subject_type: subject}
-        r = self.request('post', 'http://cleanweb-api.yandex.ru/1.0/check-spam', data=data)
+
+        try:
+            r = self.request('post', 'http://cleanweb-api.yandex.ru/1.0/check-spam', data=data)
+        except CleanwebError:
+            # Try to return fake result to hide cleanweb errors
+            return {
+                'id': -1,
+                'spam_flag': False,
+                'links': []
+            }
+
         root = ET.fromstring(r.content)
+
         return {
             'id': root.findtext('id'),
             'spam_flag': yesnobool(root.find('text').attrib['spam-flag']),
-            'links': [(link.attrib['href'], yesnobool(link.attrib['spam-flag'])) for link in root.findall('./links/link')]
+            'links': [(link.attrib['href'], yesnobool(link.attrib['spam-flag']))
+                      for link in root.findall('./links/link')]
         }
 
     def get_captcha(self, id=None):
         """ http://api.yandex.ru/cleanweb/doc/dg/concepts/get-captcha.xml"""
         payload = {'id': id}
-        r = self.request('get', 'http://cleanweb-api.yandex.ru/1.0/get-captcha', params=payload)
+
+        try:
+            r = self.request('get', 'http://cleanweb-api.yandex.ru/1.0/get-captcha', params=payload)
+        except CleanwebError:
+            if not self.fake_captcha:
+                raise KeyError("CleanWeb responed with error and we can't build fake query. "
+                               "You should set fake captcha")
+
+            return {
+                'captcha': '308JR213_g_JSaE76RvWQ3R63cK4mc8N',
+                'url': self.fake_captcha
+            }
+
         return dict((item.tag, item.text) for item in ET.fromstring(r.content))
 
     def check_captcha(self, captcha, value, id=None):
@@ -58,7 +88,11 @@ class Cleanweb(object):
         payload = {'captcha': captcha,
                    'value': value,
                    'id': id}
-        r = self.request('get', 'http://cleanweb-api.yandex.ru/1.0/check-captcha', params=payload)
+        try:
+            r = self.request('get', 'http://cleanweb-api.yandex.ru/1.0/check-captcha', params=payload)
+        except CleanwebError:
+            return True
+
         root = ET.fromstring(r.content)
         if root.findall('ok'):
             return True
@@ -67,8 +101,12 @@ class Cleanweb(object):
 
     def complain(self, id, is_spam):
         """ http://api.yandex.ru/cleanweb/doc/dg/concepts/complain.xml"""
-        r = self.request('post', 'http://cleanweb-api.yandex.ru/1.0/complain',
-                         data={'id': id, 'spamtype': 'spam' if is_spam else 'ham'})
+        try:
+            r = self.request('post', 'http://cleanweb-api.yandex.ru/1.0/complain',
+                             data={'id': id, 'spamtype': 'spam' if is_spam else 'ham'})
+        except CleanwebError:
+            pass
+
         return True
 
 
